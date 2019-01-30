6f6dc84e121c417924b61c8233d4b0a6d72e06c8
Ammar Ali <funnyguy118@gmail.com>
2019-01-30 04:00:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
-    
+    if key == "suite_a" && value.chars.first == "A"
+      return key value
   end 
 end
